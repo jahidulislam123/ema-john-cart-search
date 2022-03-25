3950958979cf5b1d0f69d14954a6bf9a7650d05a
unknown <islam15-3879@diu.edu.bd>
2022-03-25 12:49:02 +0600

    added-totalprice-tax-shipping

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props ) => {
+    // console.log(props );
+    const{cart}=props;
+    let total=0;
+    let shippint=0;
+    for(const product of cart){
+        total=total+product.price;
+        shippint=shippint+product.shipping;
+    }
+    const tax=(total*10/100).toFixed(2)
+    // tax.toFixed(4);
     return (
-        <div>
+        <div className='cart' >
             <h4>Order Summary</h4>
-                <p>Selected Items: {props.cart.length}</p>
+                <p>Selected Items: {cart.length}</p>
+                <p>Total Price:${total}</p>
+                <p>Total Shipping:${shippint}</p>
+                <p>Tax :${tax}</p>
+                <h5>Grand Total</h5>
         </div>
     );
 };
